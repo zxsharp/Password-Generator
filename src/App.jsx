8c3592348bdf29d97514502fcd5245f5920adb0b
zxsharp <ishanshdimaniya@gmail.com>
2025-02-06 23:58:36 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,166 @@
+import { useState, useMemo, memo } from 'react'
+import './App.css'
+import {password} from './password.js'
+
+function App() {
+
+  // checkboxes
+  const [smallCheck, setSmallCheck] = useState(true);
+  const [capitalCheck, setCapitalCheck] = useState(true);
+  const [numberCheck, setNumberCheck] = useState(true);
+  const [specialCheck, setSpecialCheck] = useState(true);
+
+  const [passwordLength, setPasswordLength] = useState(12);
+  const [generatedPassword, setGeneratedPassword] = useState(password(passwordLength, smallCheck, capitalCheck, numberCheck, specialCheck));
+  const [lengthErrorMessage, setLengthErrorMessage] = useState("");
+
+  function decreaseLength(){
+    if(passwordLength === 4){
+      setLengthErrorMessage("Min. length is 4");
+      return;
+    }
+    setLengthErrorMessage("");
+    setPasswordLength((prevPasswordLength) => prevPasswordLength-1);
+  }
+
+  function increaseLength(){
+    if(passwordLength === 64){
+      setLengthErrorMessage("Max. length is 64");
+      return;
+    }
+    setLengthErrorMessage("");
+    setPasswordLength((prevPasswordLength) => prevPasswordLength+1);
+  }
+
+  // update password whenever passwordLength changes
+  useMemo(() => {
+    setGeneratedPassword(password(passwordLength, smallCheck, capitalCheck, numberCheck, specialCheck));
+  }, [passwordLength, smallCheck, capitalCheck, numberCheck, specialCheck]);
+
+
+  return (
+    <>
+    <div className="heading" >
+      <Heading/>
+    </div>
+    
+    <div className="description">
+      <Description />
+    </div>
+
+    <div className="note">
+      <Note />
+    </div>
+
+    <div className="password-container">
+      <DisplayPassword generatedPassword={generatedPassword}/>
+      <CopyButton generatedPassword={generatedPassword}/>
+      <button onClick={() => setGeneratedPassword(password(passwordLength, smallCheck, capitalCheck, numberCheck, specialCheck))}>
+        Re-Generate🔄
+      </button>
+    </div>
+
+    <div className="length-container">
+      <div>
+        Length: 
+      </div>
+      <button onClick={() => decreaseLength()}>-</button>
+      <input value={passwordLength} readOnly/>
+      <button onClick={() => increaseLength()}>+</button>
+    </div>
+
+    <div className="length-error-message">{lengthErrorMessage}</div>
+
+    <div className="checkbox-container">
+      <label>
+        <input 
+          type="checkbox"
+          name="abc"
+          checked={smallCheck}
+          onChange={() => setSmallCheck((prev) => !prev)}
+        />
+        abc
+      </label>
+      <label>
+      <input 
+          type="checkbox"
+          name="ABC"
+          checked={capitalCheck}
+          onChange={() => setCapitalCheck((prev) => !prev)}
+        />
+          ABC
+      </label>
+      <label>
+      <input 
+          type="checkbox"
+          name="123"
+          checked={numberCheck}
+          onChange={() => setNumberCheck((prev) => !prev)}
+        />
+        123
+      </label>
+      <label>
+      <input 
+          type="checkbox"
+          name="@$#"
+          checked={specialCheck}
+          onChange={() => setSpecialCheck((prev) => !prev)}
+        />
+        @$#
+      </label>
+    </div>
+
+    </>
+  )
+}
+
+
+// components
+
+const Heading = memo(() => {
+  console.log("rendered heading")
+  return<div>Generate Strong Passwords</div>
+})
+
+const Description = memo(() => {
+  return <div>Secure your accounts by using strong, 100% randomly generated passwords. Make sure to save them.</div>
+})
+
+const Note = memo(() => {
+  return <div>Note - Once you refresh the page or click Re-Generate, the password will be completely lost.</div>
+})
+
+function DisplayPassword({generatedPassword}) {
+  console.log("rendered displayPassword")
+  return <>
+    <input type="text" value={generatedPassword} readOnly/>
+  </>
+}
+
+function CopyButton({generatedPassword}){
+  const [copyState, setCopyState] = useState("Copy");
+
+  // re renderes "Copy" when generatedPassword changes
+  useMemo(() => {
+    setCopyState("Copy");
+  }, [generatedPassword]);
+
+  function handleOnClick(){
+    navigator.clipboard.writeText(generatedPassword).then(() => {
+      if(copyState == "Copy"){
+        setCopyState("Copied ✅")
+      }
+      else{
+        setCopyState("Copy")
+      }
+    })
+  }
+
+  return <button onClick={handleOnClick}>{copyState}</button>
+}
+
+
+
+
+export default App
+
---
+++ src/password.js
@@ -0,0 +1,62 @@
+export function password(length, smallCheck, capitalCheck, numberCheck, specialCheck) {
+    let charSet = "";
+    let atLeastOne = [];
+
+    const smallCharSet = "abcdefghijklmnopqrstuvwxyz";
+    const capitalCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberCharSet = "0123456789";
+    const specialCharSet = "!@#$%^&*()-_";
+
+    if(smallCheck){
+        charSet += smallCharSet;
+        atLeastOne.push(smallCharSet);
+    }
+    if(capitalCheck){
+        charSet += capitalCharSet;
+        atLeastOne.push(capitalCharSet);
+    }
+    if(numberCheck){
+        charSet += numberCharSet;
+        atLeastOne.push(numberCharSet);
+    }
+    if(specialCheck){
+        charSet += specialCharSet;
+        atLeastOne.push(specialCharSet);
+    }
+
+    // if all boxes unchecked
+    if(charSet === ""){
+        return "";
+    }
+
+    let psswd = "";
+
+    for(let i=0; i<atLeastOne.length; i++){
+        psswd += atLeastOne[i][Math.floor(Math.random() * atLeastOne.length)];
+    }
+    
+    const remainingLength = length - atLeastOne.length;
+    // array of remaininglength with random values from 0 to 32-bit
+    let randomBytes = new Uint32Array(remainingLength);
+    window.crypto.getRandomValues(randomBytes);
+
+    for (let i = 0; i < remainingLength; i++) {
+        psswd += charSet[randomBytes[i] % charSet.length]; // Pick a random letter from charSet
+    }
+
+    function shuffle(str) {
+        let arr = str.split(''); 
+        // fisher-yates algo
+        for (let i = arr.length-1; i>0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+        }
+        return arr.join(''); 
+    }
+
+
+    return shuffle(psswd);
+}
+
+
+
